19edd0e4584938a3a3f79b1afb0c36c004f58683
Amita <sardana.amita2@gmail.com>
2020-11-27 21:26:49 +0530

    Add tailwind config file to extended style

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    'src/**/*.js',
+    'src/**/*.jsx',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'public/**/*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
--- tailwind.js
+++ tailwind.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.js'],
+  purge: [],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
