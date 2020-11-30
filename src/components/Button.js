5b0915625ec1ff156d40f5b8427db01f1ae32bb9
Amita <sardana.amita2@gmail.com>
2020-11-30 18:50:34 +0530

    Add default props for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class Button extends Component {
+  static defaultProps = {
+    type: 'primary',
+    block: false,
+    href: null,
+  };
+
   render() {
     return <a></a>;
   }
