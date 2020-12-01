cb0ddd00a0cac109e63275befabb5d45ea592663
Amita <sardana.amita2@gmail.com>
2020-12-01 14:21:53 +0530

    Add nav and link components

---
+++ src/components/Link.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+
+export class Link extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Link;
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Nav;
