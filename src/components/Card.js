2e846dd07a7e92375a97f0d2e2b5f3c592f81973
Amita <sardana.amita2@gmail.com>
2020-12-01 21:30:05 +0530

    Add styles for card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react';
-// import Button from './Button';
+import classNames from 'classnames';
+import Button from './Button';
 
 class Card extends Component {
   render() {
-    const { title, href } = this.props;
-    return <div></div>;
+    const cardClasses = classNames(
+      'p-12',
+      'card border-2',
+      'border-red-400',
+      'flex',
+      'flex-col',
+      'justify-between',
+      'h-96',
+      'shadow-md',
+      'hover:shadow-2xl',
+      'cursor-pointer',
+      // 'bg-any-image',
+      'bg-cover',
+      'bg-center',
+      'bg-no-repeat'
+    );
+    const { title, href, background } = this.props;
+
+    return (
+      <div
+        className={cardClasses}
+        style={{
+          backgroundImage: `linear-gradient(to bottom right, rgba(0,0,0,0.5), rgba(0,0,0,0.2), rgba(0,0,0,0.1)), url(${background})`,
+        }}
+      >
+        <div>
+          <p className="text-3xl font-display font-extrabold text-white mb-3">
+            {title}
+          </p>
+          <ul className="flex">
+            <li className="mr-2 px-2 py-1 bg-secondaryLight text-white">
+              {' '}
+              Tags
+            </li>
+            <li className="mr-2 px-2 py-1 bg-secondaryLight text-white">
+              {' '}
+              Tags
+            </li>
+            <li className="mr-2 px-2 py-1 bg-secondaryLight text-white">
+              {' '}
+              Tags
+            </li>
+            <li className="mr-2 px-2 py-1 bg-secondaryLight text-white">
+              {' '}
+              Tags
+            </li>
+          </ul>
+        </div>
+        <div>
+          <Button href={href}>See the project</Button>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,36 +1,21 @@
 import React, { Component } from 'react';
-import classNames from 'classnames';
+import Card from './Card';
 import anyImage from '../images/test.jpg';
 class Projects extends Component {
   render() {
-    const cardClasses = classNames(
-      'card border-2',
-      'border-red-400',
-      'h-96',
-      'shadow-md',
-      'hover:shadow-2xl',
-      'cursor-pointer',
-      // 'bg-any-image',
-      'bg-cover',
-      'bg-center',
-      'bg-no-repeat'
-    );
     console.log(anyImage);
     return (
       <div className="mt-20">
         <h2 className="text-3xl font-display font-extrabold">My best works</h2>
         <div className="mt-10">
           <div className="grid grid-cols-2 gap-10">
-            <div
-              className={cardClasses}
-              style={{ backgroundImage: `url(${anyImage})` }}
-            ></div>
-            <div className={cardClasses}></div>
+            <Card title="My Project" background={anyImage} />
+            <Card title="My Project" />
           </div>
           <div className="grid grid-cols-3 gap-10 mt-10">
-            <div className={cardClasses}></div>
-            <div className={cardClasses}></div>
-            <div className={cardClasses}></div>
+            <Card title="My Project" />
+            <Card title="My Project" />
+            <Card title="My Project" />
           </div>
         </div>
       </div>
