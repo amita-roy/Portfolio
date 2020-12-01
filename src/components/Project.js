97bdce752ec44040dcd5a717ea2e0140aa6c757c
Amita <sardana.amita2@gmail.com>
2020-12-01 19:42:36 +0530

    Add card shadow

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,14 @@ import classNames from 'classnames';
 
 class Project extends Component {
   render() {
-    const cardClasses = classNames('card border-2', 'border-red-400', 'h-80');
+    const cardClasses = classNames(
+      'card border-2',
+      'border-red-400',
+      'h-80',
+      'shadow-md',
+      'hover:shadow-2xl',
+      'cursor-pointer'
+    );
     return (
       <div className="mt-20">
         <h2 className="text-3xl font-display font-extrabold">My best works</h2>
