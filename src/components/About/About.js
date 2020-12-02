b3ea0d818b276a94ea5b9dee6d52b670aa7d0d4e
Amita <sardana.amita2@gmail.com>
2020-12-02 20:45:01 +0530

    Change font-color for about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -25,7 +25,7 @@ class About extends Component {
       <div className="h-96 mt-40 flex px-20">
         <div className="h-full w-1/2">
           <h2 className="text-3xl font-display font-extrabold">About Me</h2>
-          <p className="max-w-lg my-8">
+          <p className="max-w-lg my-8 text-grayLight font-normal">
             In three sentences describe who you are as a proffessional. Look
             through some of my work and experience! If you like what you see and
             have project you need coded, don’t hesistate to contact me.
