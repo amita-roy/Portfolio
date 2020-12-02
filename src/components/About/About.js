29818c7aedd9b8d814f7a0ecb4d9d174a94f9f59
Amita <sardana.amita2@gmail.com>
2020-12-02 16:57:59 +0530

    Add skill components skills

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import { Button } from '../Button';
+import { Skill } from '../Skill';
 
 class About extends Component {
   render() {
+    const languages = ['JavaScript (ES5/ES6)', 'Ruby', 'HTML', 'CSS'];
+    const frameworks = [
+      'Bootstrap',
+      'Ruby on Rails',
+      'RSpec',
+      'Capybara',
+      'Selenium',
+    ];
+    const skills = [
+      'Bootstrap',
+      'Bulma',
+      'Codekit',
+      'Codepen',
+      'Github',
+      'Gitlab',
+      'Terminal',
+    ];
     return (
-      <div className="border-2 border-red-400 h-96 mt-40 flex">
-        <div className="border-2 border-green-400 h-full w-1/2">
+      <div className="h-96 mt-40 flex">
+        <div className="h-full w-1/2">
           <h2 className="text-3xl font-display font-extrabold">About Me</h2>
           <p className="max-w-lg my-8">
             In three sentences describe who you are as a proffessional. Look
@@ -14,7 +32,11 @@ class About extends Component {
           </p>
           <Button>Get my resume</Button>
         </div>
-        <div className="border-2 border-green-400 h-full w-1/2"></div>
+        <div className="h-full w-1/2">
+          <Skill type="languages" skills={languages} />
+          <Skill type="frameworks" skills={frameworks} />
+          <Skill type="skills" skills={skills} />
+        </div>
       </div>
     );
   }
---
+++ src/components/Skill/Skill.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Skill extends Component {
+  capitalize = (s) => {
+    if (typeof s !== 'string') return '';
+    return s.charAt(0).toUpperCase() + s.slice(1);
+  };
+  render() {
+    const { type, skills } = this.props;
+    return (
+      <div className="mb-6 border-b">
+        <h3 className="text-xl font-bold font-display">
+          {this.capitalize(type)}
+        </h3>
+        <div className="flex mt-4 mb-4">
+          {skills.map((skill) => (
+            <div className="text-grayLight text-sm mr-4">{skill}</div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Skill;
