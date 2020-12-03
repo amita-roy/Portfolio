2fcf678cbe82cc319b815ae9297159c98704af74
Amita <sardana.amita2@gmail.com>
2020-12-03 15:15:35 +0530

    Add modal component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Modal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Modal;
