98506155429fae3938d8f31efe19c9791143051d
Amita <sardana.amita2@gmail.com>
2020-12-08 14:57:51 +0530

    Remove test image import

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import data from '../data/data.json';
 import { Card } from './Card';
-import anyImage from '../images/test.jpg';
 import { Modal } from './Modal';
 
 class Projects extends Component {
