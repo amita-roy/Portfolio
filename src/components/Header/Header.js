e2e6880e6d230e61b70467de92712c228a637af2
Amita <sardana.amita2@gmail.com>
2020-12-08 12:43:51 +0530

    Update the margin for Projects and Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,6 @@ import { SocialIconGroupPrimary } from '../Icon';
 export class Header extends Component {
   render() {
     const introClassnames = classNames(
-      'md:h-72',
       'flex',
       'flex-col',
       'justify-between',
@@ -16,20 +15,17 @@ export class Header extends Component {
     return (
       <div className={`${styles.intro} ${introClassnames}`} id="intro">
         <div>
-          <div>
-            <h1 className=" text-3xl md:text-5xl lg:text-6xl mb-6  md:max-w-lg font-display font-extrabold">
-              Pretty code by ingenious person
-            </h1>
-
-            <p className="w-full md:max-w-xl text-sm text-grayLight font-body">
-              Hello I am a software developer! I can help you build a product,
-              feature or website. Look through some of my work and experience!
-              If you like what you see and have project you need coded, don’t
-              hesistate to contact me.
-            </p>
-          </div>
+          <h1 className=" text-3xl md:text-5xl lg:text-6xl mb-6  md:max-w-lg font-display font-extrabold">
+            Pretty code by ingenious person
+          </h1>
+          <p className="w-full md:max-w-xl text-sm text-grayLight font-body">
+            Hello I am a software developer! I can help you build a product,
+            feature or website. Look through some of my work and experience! If
+            you like what you see and have project you need coded, don’t
+            hesistate to contact me.
+          </p>
         </div>
-        <div className="md:flex md:items-center mt-10 md:mt-0">
+        <div className="md:flex md:items-center mt-10 md:mt-20">
           <p className="sm:mr-12 text-grayLight font-semibold">
             My works in social networks
           </p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -39,7 +39,7 @@ class Projects extends Component {
         />
       ));
     return (
-      <div className="mt-10 md:mt-60 md:px-20 px-4" id="portfolio">
+      <div className="mt-10 md:mt-40 md:px-20 px-4" id="portfolio">
         <h2 className="text-3xl font-display font-extrabold">My best works</h2>
         <div className="mt-10">
           <div className="grid md:grid-cols-2 gap-10">{topProjects}</div>
