14bff2c8657df5985077d15db59747a0203df007
Amita <sardana.amita2@gmail.com>
2020-12-15 17:04:54 +0530

    Remove transition from socialIcons

--- src/components/Icon/SocialIconGroupLight.js
+++ src/components/Icon/SocialIconGroupLight.js
@@ -3,15 +3,13 @@ import { ReactComponent as Github } from '../../icons/githubLight.svg';
 import { ReactComponent as LinkedIn } from '../../icons/linkedinLight.svg';
 import { ReactComponent as Twitter } from '../../icons/twitterLight.svg';
 import { ReactComponent as Medium } from '../../icons/mediumLight.svg';
-import { ReactComponent as Insta } from '../../icons/instaLight.svg';
-import styles from './Icon.module.css';
 
 class SocialIconGroupLight extends Component {
   render() {
     return (
       <div className="flex items-center justify-evenly sm:justify-start w-full">
         <a
-          className={styles.socialIcon}
+          className="socialIcon"
           href="https://github.com/Amita-Roy"
           target="blank"
           alt="github"
@@ -19,17 +17,9 @@ class SocialIconGroupLight extends Component {
           <Github width="30" height="30" />
         </a>
         <a
-          href="https://www.instagram.com/"
-          target="blank"
-          className={`${styles.socialIcon} ml-3`}
-          alt="instagram"
-        >
-          <Insta width="30" height="30" />
-        </a>
-        <a
           href="https://www.linkedin.com/in/amita-roy/"
           target="blank"
-          className={`${styles.socialIcon} ml-3`}
+          className="socialIcon ml-3"
           alt="linkein"
         >
           <LinkedIn width="30" height="30" />
@@ -38,7 +28,7 @@ class SocialIconGroupLight extends Component {
           href="https://medium.com/@sardana.amita2"
           target="blank"
           alt="medium"
-          className={`${styles.socialIcon} ml-3`}
+          className="socialIcon ml-3"
         >
           <Medium width="30" height="30" />
         </a>
@@ -46,7 +36,7 @@ class SocialIconGroupLight extends Component {
           href="https://twitter.com/AmitaRoy14"
           target="blank"
           alt="twitter"
-          className={`${styles.socialIcon} ml-3`}
+          className="socialIcon ml-3"
         >
           <Twitter width="30" height="30" />
         </a>
--- src/components/Icon/SocialIconGroupPrimary.js
+++ src/components/Icon/SocialIconGroupPrimary.js
@@ -3,8 +3,6 @@ import { ReactComponent as Github } from '../../icons/github.svg';
 import { ReactComponent as LinkedIn } from '../../icons/linkedin.svg';
 import { ReactComponent as Twitter } from '../../icons/twitter.svg';
 import { ReactComponent as Medium } from '../../icons/medium.svg';
-import { ReactComponent as Insta } from '../../icons/insta.svg';
-import styles from './Icon.module.css';
 
 class SocialIconGroupPrimary extends Component {
   render() {
@@ -13,23 +11,15 @@ class SocialIconGroupPrimary extends Component {
         <a
           href="https://github.com/Amita-Roy"
           target="blank"
-          className={styles.socialIcon}
+          className="socialIcon"
           alt="github"
         >
           <Github width="30" height="30" />
         </a>
         <a
-          href="https://www.instagram.com/"
-          target="blank"
-          className={`${styles.socialIcon} ml-3`}
-          alt="instagram"
-        >
-          <Insta width="30" height="30" />
-        </a>
-        <a
           href="https://www.linkedin.com/in/amita-roy/"
           target="blank"
-          className={`${styles.socialIcon} ml-3`}
+          className="socialIcon ml-3"
           alt="linkedin"
         >
           <LinkedIn width="30" height="30" />
@@ -38,7 +28,7 @@ class SocialIconGroupPrimary extends Component {
           href="https://medium.com/@sardana.amita2"
           target="blank"
           alt="medium"
-          className={`${styles.socialIcon} ml-3`}
+          className="socialIcon ml-3"
         >
           <Medium width="30" height="30" />
         </a>
@@ -46,7 +36,7 @@ class SocialIconGroupPrimary extends Component {
           href="https://twitter.com/AmitaRoy14"
           target="blank"
           alt="twitter"
-          className={`${styles.socialIcon} ml-3`}
+          className="socialIcon ml-3"
         >
           <Twitter width="30" height="30" />
         </a>
